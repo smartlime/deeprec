cf704b2175f2fb2fb6288d390b3ae1e19e831b56
geneux <e@varnakov.ru>
2016-04-27 02:52:51 +0300

    Validators and green Spec for Question model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending it { should validate_presence_of :topic }
-  pending it { should validate_presence_of :body }
-  pending it { should validate_presence_of :rating }
+  it { should validate_presence_of :topic }
+  it { should validate_presence_of :body }
 
-  pending it { should validate_length_of(:topic).is_at_least(10) }
-  pending it { should validate_length_of(:topic).is_at_most(50) }
-  pending it { should validate_length_of(:body).is_at_least(20) }
-  pending it { should validate_length_of(:body).is_at_most(8192) }
+  it { should validate_length_of(:topic).is_at_least(10) }
+  it { should validate_length_of(:topic).is_at_most(50) }
+  it { should validate_length_of(:body).is_at_least(20) }
+  it { should validate_length_of(:body).is_at_most(8192) }
+
+  it { should validate_numericality_of(:rating).only_integer }
+  it { should validate_numericality_of(:rating).is_equal_to(0).on(:create) }
 
   it { should have_db_index :rating}
 end
