2db90e658b27979965a520e6fabf1e47b5b0a8cc
geneux <e@varnakov.ru>
2016-04-27 05:04:26 +0300

    Validators and green spec for the Answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
 
-  it { pending; should validate_presence_of :body }
+  it { should validate_presence_of :body }
 
-  it { pending; should validate_length_of(:body).is_at_least(20) }
-  it { pending; should validate_length_of(:body).is_at_most(8192) }
+  it { should validate_length_of(:body).is_at_least(20) }
+  it { should validate_length_of(:body).is_at_most(8192) }
 
-  it { pending; should validate_numericality_of(:rating).only_integer }
-  it { pending; should validate_numericality_of(:rating).is_equal_to(0).on(:create) }
+  it { should validate_numericality_of(:rating).only_integer }
+  it { should validate_numericality_of(:rating).is_equal_to(0).on(:create) }
 
   it { should have_db_index :question_id }
   it { should have_db_index :rating }
