f4e9d4c3a792b89fbac9b28e66a521040fdfe684
geneux <e@varnakov.ru>
2016-04-27 03:54:00 +0300

    Spec for Answer model (Y/G)

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :question }
+
+  it { pending; should validate_presence_of :body }
+
+  it { pending; should validate_length_of(:body).is_at_least(20) }
+  it { pending; should validate_length_of(:body).is_at_most(8192) }
+
+  it { pending; should validate_numericality_of(:rating).only_integer }
+  it { pending; should validate_numericality_of(:rating).is_equal_to(0).on(:create) }
+
+  it { should have_db_index :question_id }
+  it { should have_db_index :rating }
 end
