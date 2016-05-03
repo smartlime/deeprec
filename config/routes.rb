7a7efbdc22a512fd24f7567e2f61882ac15f3ed9
geneux <e@varnakov.ru>
2016-05-03 22:12:34 +0300

    Add nested route for Answers, restrict actions in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'questions#index'
 
-  resources :questions
+  resources :questions, only: [:index, :show, :new, :create] do
+    resources :answers, only: [:new, :create]
+  end
 end
