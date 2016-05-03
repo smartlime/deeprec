bf81a9b7149060da12feb0a937b3748c6e134818
geneux <e@varnakov.ru>
2016-05-03 19:45:06 +0300

    Config factory_girl as fixture generator

--- config/application.rb
+++ config/application.rb
@@ -37,12 +37,13 @@ module DeepRecursion
 
     config.generators do |g|
       g.test_framework :rspec,
-          fixtures: true,
-          view_specs: false,
-          helper_specs: false,
-          routing_specs: false,
-          request_specs: false,
-          controller_specs: true
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_specs: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
   end
 end
