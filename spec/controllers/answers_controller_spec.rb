b96d17a182ec82c6f165260792ebf445ba9595be
geneux <e@varnakov.ru>
2016-05-03 23:33:05 +0300

    Yellow stub for answers_controller_spec

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   describe 'POST #create' do
-    xit 'stores new answer in the database'
+
+    context 'with valid attributes' do
+      xit 'stores new answer in the database'
+      xit 'redirects to show Questions view'
+    end
+
+    context 'with invalid attributes' do
+      xit 'doesn\'t store the question'
+      xit 'redirects to show Questions view'
+    end
   end
 end
