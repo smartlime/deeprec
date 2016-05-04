47b0dfaa84c65497948b59f96121d49385bddd60
geneux <e@varnakov.ru>
2016-05-04 17:18:32 +0300

    Green test for answer_controller when create with valid attributes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,22 @@
 class AnswersController < ApplicationController
+  before_action :get_question_id
+
+  def create
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      redirect_to question_path(@question_id)
+    else
+      redirect_to question_path(@question_id)
+    end
+  end
+
+  private
+
+  def get_question_id
+    @question_id = params[:question_id]
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body).merge(question_id: @question_id)
+  end
 end
