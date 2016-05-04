b56225d19a55a29b0e242a6b90ca8dd76cdeb8f8
geneux <e@varnakov.ru>
2016-05-04 15:37:44 +0300

    Change specs to check Answers array in @question object (not sure if this is correct check)

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe QuestionsController, type: :controller do
 
     before { get :index }
 
-    it 'populates an array of all questions' do
+    it 'populates an array of all Questions' do
       expect(assigns(:questions)).to match_array(questions)
     end
 
-    it 'renders index view' do
+    it 'renders #index view' do
       expect(response).to render_template :index
     end
   end
@@ -22,19 +22,19 @@ RSpec.describe QuestionsController, type: :controller do
 
     before { get :show, id: question }
 
-    it 'assings the requested question to @question' do
+    it 'assings the requested Question to @question' do
       expect(assigns(:question)).to eq question
     end
 
-    it 'populates an array of all answers to @question' do
-      expect(assigns(:answers)).to match_array(answers)
+    it '@question includes array with Answers' do
+      expect(assigns(:question).answers.all).to eq answers
     end
 
     it 'assigns a new Answer to @answer' do
       expect(assigns(:answer)).to be_a_new(Answer)
     end
 
-    it 'renders show view' do
+    it 'renders #show view' do
       expect(response).to render_template :show
     end
   end
@@ -46,18 +46,18 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:question)).to be_a_new(Question)
     end
 
-    it 'renders new view' do
+    it 'renders #new view' do
       expect(response).to render_template :new
     end
   end
 
   describe 'POST #create' do
     context 'with valid attributes' do
-      it 'stores new question in the database' do
+      it 'stores new Question in the database' do
         expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
       end
 
-      it 'redirects to show view' do
+      it 'redirects to #show view' do
         post :create, question: attributes_for(:question)
         expect(response).to redirect_to question_path(assigns(:question))
       end
@@ -68,7 +68,7 @@ RSpec.describe QuestionsController, type: :controller do
         expect { post :create, question: attributes_for(:invalid_question) }.to_not change(Question, :count)
       end
 
-      it 're-renders new view' do
+      it 're-renders #new view' do
         post :create, question: attributes_for(:invalid_question)
         expect(response).to render_template :new
       end
