0d9acd5fc4d39dd0e620a46aff32d3f2b60ebe05
geneux <e@varnakov.ru>
2016-05-04 22:29:45 +0300

    Remove unneded test

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -26,10 +26,6 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:question)).to eq question
     end
 
-    it '@question includes array with Answers' do
-      expect(assigns(:question).answers.all).to eq answers
-    end
-
     it 'renders #show view' do
       expect(response).to render_template :show
     end
