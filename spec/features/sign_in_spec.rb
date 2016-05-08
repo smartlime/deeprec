8e353083f546e173015cb0bd1ab2d46805e697f8
geneux <e@varnakov.ru>
2016-05-08 03:01:57 +0300

    Add user sign in spec

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'Signing in', %{
+  To ask questions
+  As an user
+  I want to sign in
+ } do
+
+  given(:user) { create(:user) }
+
+  scenario 'Registered user tries to sign in' do
+    sign_in(user)
+
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'Not registered user tries to sign in' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'someotheruser@homehost.dom'
+    fill_in 'Password', with: 'imaginary-password'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password.'
+  end
+end
