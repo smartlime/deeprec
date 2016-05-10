3e38528df1d7e860943ff10a01df36cd49cb7510
geneux <e@varnakov.ru>
2016-05-10 14:50:02 +0300

    Add sign_out_spec

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,21 @@
+# Welcome! You have signed up successfully.
+require 'rails_helper'
+
+feature 'Signing up', %(
+  To give answers to the questions
+  As an unregistered user
+  I want to sign up
+) do
+
+  given(:password) { Faker::Internet.password }
+
+  scenario 'New user tries to sign up' do
+    visit new_user_registration_path
+    fill_in 'Email', with: Faker::Internet.email
+    fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+end
