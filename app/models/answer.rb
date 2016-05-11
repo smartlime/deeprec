ef64acad67c0428dc296eeb2fc158489bdbf124a
geneux <e@varnakov.ru>
2016-05-12 00:43:25 +0300

    Merge 'presence' validators for Answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,6 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
 
-  validates :user_id, presence: true
-  validates :question_id, presence: true
+  validates :user_id, :question_id, presence: true
   validates :body, presence: true, length: (20..50_000)
 end
