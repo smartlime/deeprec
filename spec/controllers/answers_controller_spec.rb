c71eafccdc159e5c5d062880ecac22dfbc5f1221
dave <dave@deep.msk.ru>
2016-06-28 00:26:52 +0300

    Implement update Answer in controller and its spec

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   let(:user) { create(:user) }
-  let(:question) { create(:question, user: user) }
+  let!(:question) { create(:question, user: user) }
 
   describe 'POST #create' do
     sign_in_user
@@ -10,17 +10,17 @@ RSpec.describe AnswersController, type: :controller do
     context 'with valid attributes' do
       it 'stores new Answer in the database' do
         expect { post :create, question_id: question.id, answer: attributes_for(:answer), format: :js }.
-          to change(Answer, :count).by(1)
+            to change(Answer, :count).by(1)
       end
 
       it 'associates new Answer with correct Question' do
         expect { post :create, question_id: question.id, answer: attributes_for(:answer), format: :js }.
-          to change(question.answers, :count).by(1)
+            to change(question.answers, :count).by(1)
       end
 
       it 'associates new Answer with correct User' do
         expect { post :create, question_id: question.id, answer: attributes_for(:answer), format: :js }.
-          to change(@user.answers, :count).by(1)
+            to change(@user.answers, :count).by(1)
       end
 
       it 'renders #create partial' do
@@ -37,12 +37,48 @@ RSpec.describe AnswersController, type: :controller do
     context 'with invalid attributes' do
       it 'doesn\'t store the Answer' do
         expect { post :create, question_id: question.id, answer: attributes_for(:invalid_answer), format: :js }.
-          to_not change(Answer, :count)
+            to_not change(Answer, :count)
       end
 
       it 'renders to #create partial' do
         post :create, question_id: question.id, answer: attributes_for(:invalid_answer), format: :js
         expect(response).to render_template :create
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'own answer' do
+      before { sign_in user }
+      let(:answer) { create(:answer, question: question) }
+
+      it 'assigns answer to edit to @answer' do
+        patch :update, id: answer, question_id: question, answer: attributes_for(:answer), user: user, format: :js
+        expect(assigns(:answer)).to eq answer
+      end
+
+      it 'changes answer attributes' do
+        edited_body = Faker::Lorem.paragraph(4, true, 8)
+        patch :update, id: answer, question_id: question, answer: {body: edited_body}, user: user, format: :js
+        answer.reload
+        expect(answer.body).to eq edited_body
+      end
+
+      it 'renders #update partial' do
+        patch :update, id: answer, question_id: question, answer: attributes_for(:answer), user: user, format: :js
+        expect(response).to render_template :update
+      end
+    end
+
+    context 'other user\'s answer' do
+      before { sign_in user }
+      before { @alt_answer = create(:answer, question: question, user: create(:user)) }
+
+      it 'doesn\'t change answer attributes' do
+        edited_body = Faker::Lorem.paragraph(4, true, 8)
+        patch :update, id: @alt_answer, question_id: question, answer: {body: edited_body}, user: user, format: :js
+        @alt_answer.reload
+        expect(@alt_answer.body).to eq @alt_answer.body
       end
     end
   end
@@ -56,7 +92,7 @@ RSpec.describe AnswersController, type: :controller do
       it 'deletes answer' do
         answer
         expect { delete :destroy, question_id: question, id: answer }.
-          to change(Answer, :count).by(-1)
+            to change(Answer, :count).by(-1)
       end
 
       it 'redirects to question#show view' do
@@ -76,7 +112,7 @@ RSpec.describe AnswersController, type: :controller do
 
       it 'doesn\'t delete answer' do
         expect { delete :destroy, question_id: question, id: @alt_answer }.
-          to change(Answer, :count).by(0)
+            to change(Answer, :count).by(0)
       end
 
       it 'keeps answer in DB' do
