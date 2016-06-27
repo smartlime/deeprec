3c0c5ba98855378545f63c0f12ecc8486f8992e4
dave <dave@deep.msk.ru>
2016-06-27 18:46:29 +0300

    WIP new specs

---
+++ spec/features/edit_answer_spec.rb
@@ -0,0 +1,18 @@
+require 'features_helper'
+
+feature 'User can edit own answer to any question', %(
+  To be able to correct mistake in answer or give an extra information
+  As an anthenticated user
+  I want to edit my own answer to any question
+) do
+
+  scenario 'Authenticated user can access edit button for his own answer to any question'
+  scenario 'Authenticated user can edit his own answer to any question'
+  scenario ''
+  scenario ''
+  scenario ''
+  scenario ''
+  scenario ''
+  scenario ''
+
+end
