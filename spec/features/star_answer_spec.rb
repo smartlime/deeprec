d7cb6f69edd5e2492d007820d65ebaabfbc27f32
dave <dave@deep.msk.ru>
2016-07-03 14:17:15 +0300

    Implement case when User can star own Answers

--- spec/features/star_answer_spec.rb
+++ spec/features/star_answer_spec.rb
@@ -1,13 +1,82 @@
 require 'features_helper'
 
 feature 'User can star the best answer to his question', %(
-  To be able to choose best answer
+  To be able to show others the best answer
   As an anthenticated user
-  I want to start any answer to my own question
+  I want to star any answer to my own question
 ) do
-  scenario 'Authenticated user can star any answer to his own question'
-  scenario 'Authenticated user can star any other answer to his own question if there starred answer is already exists'
-  scenario 'Authenticated user cannot star an answer to other user\'s question'
-  scenario 'Starred answer is always first in answers list'
-  scenario 'Unauthenticated user cannot star any answer to any question'
+  given(:user) { create(:user) }
+  given(:question) { create(:question, user: user) }
+  given!(:answer) { create(:answer, question: question, user: user) }
+
+  describe 'Authenticated User' do
+    before do
+      sign_in user
+      visit question_path(question)
+    end
+
+    context 'for his own Answer to any Question' do
+      scenario 'can access star button' do
+        within "#answer-#{answer.id}" do
+          expect(page).to have_link 'Star'
+        end
+      end
+
+      context 'clicking the star button when there\'s no starred answer' do
+        before do
+          within("#answer-#{answer.id}") { click_on 'Star' }
+        end
+
+        scenario 'can star any Answer', js: true do
+          within "#answer-#{answer.id}" do
+            expect(page).to_not have_link 'Star'
+            expect(page).to have_content answer.body
+            expect(page).to have_content 'Best Answer:'
+          end
+        end
+      end
+    end
+
+    scenario 'starred Answer is always at the top of Answers list', js: true do
+      second_answer = create(:answer, question: question, user: user)
+      visit question_path(question)
+      within("#answer-#{second_answer.id}") { click_on 'Star' }
+      within "#answers" do
+        expect(page.first('div')[:id]).to eq "answer-#{second_answer.id}"
+      end
+    end
+
+    scenario 'can star any other answer if there starred answer is already exists', js: true do
+      alt_answer = create(:answer, question: question, user: user, starred: true)
+
+      visit question_path(question)
+
+      within("#answer-#{answer.id}") do
+        click_on 'Star'
+        expect(page).to_not have_link 'Star'
+        expect(page).to have_content 'Best Answer:'
+      end
+    end
+
+    scenario 'cannot see star button at answer to other user\'s question', js: true do
+      alt_user = create(:user)
+      alt_question = create(:question, user: alt_user)
+      alt_answer = create(:answer, question: alt_question, user: user)
+
+      visit question_path(alt_question)
+      alt_answer.reload
+
+      within("#answer-#{alt_answer.id}") do
+        expect(page).to_not have_link 'Star'
+      end
+    end
+  end
+
+  scenario 'Unauthenticated user cannot access star button for any answer to any question' do
+    visit question_path(question)
+    within "div#answers" do
+      expect(page).to_not have_link 'Star'
+    end
+  end
+
 end
