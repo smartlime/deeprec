ebb0d5e8120b275126ca4c1fc158a54e9a435caf
dave <dave@deep.msk.ru>
2016-07-10 22:04:40 +0300

    Implement delete Answer Attacments

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -3,4 +3,9 @@ FactoryGirl.define do
     file { Rack::Test::UploadedFile.new(File.join(Rails.root, 'README.md')) }
     association :attachable, factory: :question
   end
+
+  factory :answer_attachment, class: 'Attachment' do
+    file { Rack::Test::UploadedFile.new(File.join(Rails.root, 'README.md')) }
+    association :attachable, factory: :answer
+  end
 end
--- spec/features/delete_attachment_from_answer_spec.rb
+++ spec/features/delete_attachment_from_answer_spec.rb
@@ -6,17 +6,40 @@ feature 'User can delete own Answer\'s attachments', %q(
   I want to delete an Attachment to the Answer
 ) do
   given(:user) { create(:user) }
+  given(:question) { create(:question, user: user) }
+  given(:answer) { create(:answer, question: question, user: user) }
+  given!(:attachment) { create(:answer_attachment, attachable: answer) }
 
   describe 'Authenticated User' do
-    before do
+    scenario 'can delete own Attachment to the Answer', :js do
       sign_in user
       visit question_path(question)
+
+      within '#answers' do
+        expect(page).to have_link attachment.file.filename
+        find("#delete-attachment-#{attachment.id}").click
+        expect(page).to_not have_link attachment.file.filename
+      end
     end
 
-    scenario 'can delete own Attachment to the Answer'
-    scenario 'cannot delete other User\'s Attachment to the Answer'
+    scenario 'cannot see delete link for other User\'s Attachment to the Answer' do
+      @alt_user = create(:user)
+      sign_in @alt_user
+      visit question_path(question)
+
+      within '#answers' do
+        expect(page).to have_link attachment.file.filename
+        expect(page).to_not have_link "#delete-attachment-#{attachment.id}"
+      end
+    end
   end
 
-  scenario 'Unauthenticated User cannot see delete attachment link'
+  scenario 'Unauthenticated User cannot see delete attachment link' do
+    visit question_path(question)
 
+    within '#answers' do
+      expect(page).to have_link attachment.file.filename
+      expect(page).to_not have_link "#delete-attachment-#{attachment.id}"
+    end
   end
+end
