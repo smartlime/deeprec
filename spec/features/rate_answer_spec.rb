7fe098e392fb8de42f80784c534c5a41a2502046
Evgeny Varnakov <e@varnakov.ru>
2016-07-16 21:04:33 +0300

    Implement Ratings for Answers (with green feature specs)

--- spec/features/rate_answer_spec.rb
+++ spec/features/rate_answer_spec.rb
@@ -5,12 +5,93 @@ feature 'Rate the Answer', %q(
   As a User
   I want to rate other Users' Answers and revoke my votes
 ) do
+  given(:user) { create(:user) }
+  given(:other_user) { create(:user) }
+  given(:question) { create(:question, user: user) }
+  given!(:answer) { create(:answer, question: question, user: other_user) }
+
   describe 'Authenticated User' do
-    scenario 'Can see Answer Rating'
-    scenario 'Can rate other User\'s Answer'
-    scenario 'Cannot see links to rate other User\'s Answer second time'
-    scenario 'Cannot see links to rate own Answer'
-    scenario 'Can revoke own vote and rate other Answer'
+    before do
+      sign_in user
+      visit question_path(question)
+    end
+
+    scenario 'can see Answer Rating' do
+      within "#answer-#{answer.id}" do
+        expect(page).to have_content 'Rating: '
+      end
+    end
+
+    scenario 'can rate other User\'s Answer' do
+      within "#answer-#{answer.id}" do
+        expect(page).to have_link 'GLYPH:plus-sign'
+        expect(page).to have_link 'GLYPH:minus-sign'
+      end
+    end
+
+    scenario 'can increment rate', :js do
+      within "#answer-#{answer.id}" do
+        click_on 'GLYPH:plus-sign'
+      end
+
+      within "#rating-#{answer.id}" do
+        expect(page).to have_content '1'
+      end
+    end
+
+    scenario 'can decrement rate', :js do
+      within "#answer-#{answer.id}" do
+        click_on 'GLYPH:minus-sign'
+      end
+
+      within "#rating-#{answer.id}" do
+        expect(page).to have_content '-1'
+      end
+    end
+
+    scenario 'cannot see links to rate other User\'s Answer second time', :js do
+      within "#answer-#{answer.id}" do
+        click_on 'GLYPH:plus-sign'
+        expect(page).to_not have_link 'GLYPH:plus-sign'
+        expect(page).to_not have_link 'GLYPH:minus-sign'
+      end
+    end
+
+    scenario 'cannot see links to rate own Answer' do
+      alt_answer = create(:answer, question: question, user: user)
+      visit question_path(question)
+
+      within "#answer-#{alt_answer.id}" do
+        expect(page).to_not have_link 'GLYPH:plus-sign'
+        expect(page).to_not have_link 'GLYPH:minus-sign'
+      end
+    end
+
+    scenario 'can revoke own vote and rate other Answer', :js do
+      alt_answer = create(:answer, question: question)
+      visit question_path(question)
+
+      within "#answer-#{answer.id}" do
+        click_on 'GLYPH:plus-sign'
+        expect(page).to have_link 'GLYPH:remove-circle'
+
+        click_on 'GLYPH:remove-circle'
+      end
+
+      within "#answer-#{alt_answer.id}" do
+        expect(page).to have_link 'GLYPH:plus-sign'
+        expect(page).to have_link 'GLYPH:minus-sign'
+      end
+    end
   end
-  scenario 'Unauthenticated user cannot see links to rate the Answer'
+
+  scenario 'Unauthenticated user cannot see links to rate the Answer' do
+    visit question_path(question)
+
+    within '#answers' do
+      expect(page).to have_content 'Rating: '
+      expect(page).to_not have_link 'GLYPH:plus-sign'
+      expect(page).to_not have_link 'GLYPH:minus-sign'
+    end
+  end
 end
