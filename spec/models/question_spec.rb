670239d44d5ee6380265173301b2a4bf485705d1
Evgeny Varnakov <e@varnakov.ru>
2016-07-18 22:05:18 +0300

    Change 'should' to 'is_expected.to' in models

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Question do
-  it { should belong_to :user }
-  it { should have_many(:answers).dependent(:destroy) }
-  it { should have_many(:attachments).dependent(:destroy) }
-  it { should have_many(:ratings).dependent(:destroy) }
+  it { is_expected.to belong_to :user }
+  it { is_expected.to have_many(:answers).dependent(:destroy) }
+  it { is_expected.to have_many(:attachments).dependent(:destroy) }
+  it { is_expected.to have_many(:ratings).dependent(:destroy) }
 
-  it { should accept_nested_attributes_for :attachments }
+  it { is_expected.to accept_nested_attributes_for :attachments }
 
-  it { should validate_presence_of :user_id }
-  it { should validate_presence_of :topic }
-  it { should validate_presence_of :body }
+  it { is_expected.to validate_presence_of :user_id }
+  it { is_expected.to validate_presence_of :topic }
+  it { is_expected.to validate_presence_of :body }
 
-  it { should validate_length_of(:topic).is_at_least(10) }
-  it { should validate_length_of(:topic).is_at_most(200) }
-  it { should validate_length_of(:body).is_at_least(20) }
-  it { should validate_length_of(:body).is_at_most(50_000) }
+  it { is_expected.to validate_length_of(:topic).is_at_least(10) }
+  it { is_expected.to validate_length_of(:topic).is_at_most(200) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
+  it { is_expected.to validate_length_of(:body).is_at_most(50_000) }
 
-  it { should have_db_index :user_id }
+  it { is_expected.to have_db_index :user_id }
 
   let (:user) { create(:user) }
   let (:question) { create(:question, user: user) }
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 describe Rating do
-  it { should belong_to :user }
-  it { should belong_to :rateable }
+  it { is_expected.to belong_to :user }
+  it { is_expected.to belong_to :rateable }
 
-  it { should validate_presence_of :user_id }
-  it { should validate_presence_of :rateable_id }
-  it { should validate_presence_of :rateable_type }
+  it { is_expected.to validate_presence_of :user_id }
+  it { is_expected.to validate_presence_of :rateable_id }
+  it { is_expected.to validate_presence_of :rateable_type }
 
-  it { should validate_inclusion_of(:rate).in_array([1, -1]) }
+  it { is_expected.to validate_inclusion_of(:rate).in_array([1, -1]) }
 
-  it { should have_db_index [:rateable_id, :rateable_type] }
-  it { should have_db_index [:user_id, :rateable_id, :rateable_type] }
+  it { is_expected.to have_db_index [:rateable_id, :rateable_type] }
+  it { is_expected.to have_db_index [:user_id, :rateable_id, :rateable_type] }
 end
