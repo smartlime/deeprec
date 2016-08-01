5eafc67d459695af30dd39b1fe13ee7029610762
Evgeny Varnakov <e@varnakov.ru>
2016-08-01 21:06:27 +0300

    Move Rateable model specs to shared exmples

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Question do
+describe Question do
   it { is_expected.to belong_to :user }
   it { is_expected.to have_many(:answers).dependent(:destroy) }
   it { is_expected.to have_many(:attachments).dependent(:destroy) }
@@ -19,43 +19,10 @@ RSpec.describe Question do
 
   it { is_expected.to have_db_index :user_id }
 
-  let (:user) { create(:user) }
-  let (:question) { create(:question, user: user) }
-  describe '#rate_up! and #rating' do
-    it 'should store rate' do
-      expect { question.rate_up!(user) }.to change(question.ratings, :count).by(1)
-    end
-
-    it 'should increase rate' do
-      expect { question.rate_up!(user) }.to change { question.rating }.by(1)
-    end
-  end
-
-  describe '#rate_down! and #rating' do
-    it 'should store rate' do
-      expect { question.rate_down!(user) }.to change(question.ratings, :count).by(1)
-    end
-
-    it 'should decrease rate' do
-      expect { question.rate_down!(user) }.to change { question.rating }.by(-1)
-    end
-  end
-
-  describe '#revoke_rate! and #rating' do
-    it 'should revorke rate' do
-      question.rate_up!(user)
-      expect { question.revoke_rate!(user) }.to change { question.rating }.to(0)
-    end
-  end
-
-  describe '#rated? and #rate_up!' do
-    it 'should have the question not rated initially' do
-      expect(question.rated?(question, user)).to eq false
-    end
-
-    it 'should mark question as rated after #change_rate' do
-      expect { question.rate_up!(user) }
-          .to change { question.rated?(question, user) }.to(true)
+  describe 'acts as Rateable' do
+    include_examples :rateable do
+      let (:user) { create(:user) }
+      let (:rateable) { create(:question, user: user) }
     end
   end
 end
