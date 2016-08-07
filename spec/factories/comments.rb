a4399f0c0a9d6b4f8d6d1b661d5e35aa14be8072
Evgeny Varnakov <e@varnakov.ru>
2016-08-08 00:46:24 +0300

    Implement Question Comments in draft w/Comet wo/specs

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
   factory :comment do
-    
+    user
+    body { Faker::Lorem.paragraph(4, true, 8) }
+  end
+
+  factory :invalid_comment, class: 'Comment' do
+    user nil
+    body nil
   end
 end
