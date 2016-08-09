4018145063c1384a7f3a10772e314b645475bd60
Evgeny Varnakov <e@varnakov.ru>
2016-08-10 00:31:41 +0300

    Add comments_controller's specs (green) for both Q and A

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,14 +1,55 @@
 require 'rails_helper'
 
-describe CommentsController do
-  let(:user) { create(:user) }
-  let(:question) { create(:question, user: user) }
+shared_examples :commented do
+  let(:post_comment) { post :create, commentable: commentable.entity.pluralize,
+      "#{commentable.entity}_id": commentable.id,
+      comment: attributes_for(:comment), format: :js }
+  let(:post_invalid_comment) { post :create, commentable: commentable.entity.pluralize,
+      "#{commentable.entity}_id": commentable.id,
+      comment: attributes_for(:invalid_comment), format: :js }
 
+  describe 'POST #create' do
+    sign_in_user
+
+    it 'assigns @commentable' do
+      post_comment
+      expect(assigns(:commentable)).to eq commentable
+    end
+
+    context 'with valid attributes' do
+      it 'stores Comment in the database and associates with correct Commetable' do
+        expect { post_comment }.to change(commentable.comments, :count).by(1)
+      end
+
+      it 'associates Comment with correct User' do
+        expect { post_comment }.to change(@user.comments, :count).by(1)
+      end
+
+      subject { post_comment }
+      it { is_expected.to render_template :create }
+    end
+
+    context 'with invalid attributes' do
+      it 'doesn\'t store Comment in the database' do
+        expect { post_invalid_comment }.to_not change(Comment, :count)
+      end
+
+      subject { post_invalid_comment }
+      it { is_expected.to render_template :create }
+    end
+  end
+end
+
+describe CommentsController do
   context 'Comments for Questions' do
     include_examples :commented do
-      let(:commentable) { question }
-      let(:post_commentable) { post :create, commentable: 'questions',
-          question_id: question.id, comment: attributes_for(:comment), format: :js }
+      let(:commentable) { create(:question) }
+    end
+  end
+
+  context 'Comments for Answers' do
+    include_examples :commented do
+      let(:commentable) { create(:answer) }
     end
   end
 end
