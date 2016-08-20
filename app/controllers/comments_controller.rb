c1065b834036ea7c5ab4e6aaf9115aace67dc187
Evgeny Varnakov <e@varnakov.ru>
2016-08-20 21:57:41 +0300

    Refactor CommentsController to detect commentable automatically

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,19 +5,14 @@ class CommentsController < ApplicationController
   respond_to :js
 
   def create
-    # entity = params[:commentable].singularize
-    # @commentable = entity.classify.constantize.find(params["#{entity}_id"])
-    # @comment = @commentable.comments.build(params.require(:comment).permit(:body))
-    # @comment.user_id = current_user.id
-    # @comment.save
     respond_with(@comment = @commentable.comments.create(comment_params))
   end
 
   private
 
   def load_commentable
-    @commentable = Question.find(params[:question_id]) if params[:question_id]
-    @commentable ||= Answer.find(params[:answer_id])
+    entity = request.path.split('/').second.singularize
+    @commentable = entity.classify.constantize.find(params["#{entity}_id"])
   end
 
   def comment_params
