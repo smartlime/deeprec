f85e9ae4f70d668331ef7e49ccddbd18d26e5e8c
dave <dave@deep.msk.ru>
2016-08-23 16:27:41 +0300

    Add Twitter in callbacks controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,7 +7,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  # def twitter
-  #   #TODO: twitter oauth
-  # end
+  def twitter
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message :notice, :success, kind: 'Twitter' if is_navigational_format?
+    end
+  end
 end
