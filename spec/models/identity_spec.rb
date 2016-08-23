cfc6b6d30798d4151ace61f89dd00ede3732049c
dave <dave@deep.msk.ru>
2016-08-23 16:28:23 +0300

    Add Identity model validations and specs

--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Identity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Identity do
+  it { is_expected.to belong_to :user }
+
+  it { is_expected.to validate_presence_of :user_id }
+  it { is_expected.to validate_presence_of :uid }
+  it { is_expected.to validate_presence_of :provider }
+
+  describe 'should validate uniqueness of uid for the provider' do
+    subject { create(:alt_identity) }
+    it { is_expected.to validate_uniqueness_of(:uid).scoped_to(:provider) }
+  end
 end
