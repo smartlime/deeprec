bc9c1b5f4d55da19dfe958c40512d0833744ab6c
Evgeny Varnakov <e@varnakov.ru>
2016-09-06 03:04:01 +0300

    Draft of omniauth_callbacks controller w/spec

---
+++ spec/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+shared_examples :oauth_callback do
+  let(:user) { create(:user) }
+  before { request.env['devise.mapping'] = Devise.mappings[:user] }
+
+
+  context 'empty call' do
+    subject { get provider }
+    it { is_expected.to redirect_to new_user_registration_path }
+    # if { is_expected.to show_flas}
+  end
+
+  context 'email is nil' do
+    before do
+      request.env['omniauth.auth'] = OmniAuth::AuthHash.new(provider: provider.to_s, uid: '123456', info: {email: nil})
+      get provider
+    end
+
+    it 'stores data in session' do
+      expect(session['devise.oauth_provider']).to eq provider.to_s
+      expect(session['devise.oauth_uid']).to eq '123456'
+    end
+
+    it { should_not be_user_signed_in }
+    it { expect(response).to redirect_to 'user/email' }
+  end
+
+  context 'user does not exist' do
+    before do
+      request.env['omniauth.auth'] = OmniAuth::AuthHash.new(provider: provider.to_s, uid: '123456', info: {email: 'new-user@email.com'})
+      get provider
+    end
+
+    it 'assigns user to @user' do
+      expect(assigns(:user)).to be_a(User)
+    end
+    it 'expects to send confirmation email'
+  end
+
+  context 'everything is nil' do
+    before do
+      request.env['omniauth.auth'] = OmniAuth::AuthHash.new(provider: nil, uid: nil, info: {email: nil})
+      get provider
+    end
+
+    it 'redirect to registration' do
+      expect(response).to redirect_to new_user_registration_path
+    end
+  end
+end
+
+describe OmniauthCallbacksController do
+
+  [:facebook, :twitter].each do |provider|
+    describe "GET ##{provider}" do
+      include_examples :oauth_callback do
+        let(:provider) { provider }
+      end
+    end
+  end
+end
