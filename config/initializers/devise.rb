0f728f85ee9e819cabc171f6985e6b39d297e06d
dave <dave@deep.msk.ru>
2016-09-06 18:47:40 +0300

    Add Twitter OAuth to Devise config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -245,6 +245,8 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, Rails.application.secrets.facebook_app_id,
                   Rails.application.secrets.facebook_app_secret, scope: [:email]
+  config.omniauth :twitter, Rails.application.secrets.twitter_app_id,
+                  Rails.application.secrets.twitter_app_secret
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
