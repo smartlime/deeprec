eba6cb807c9a2e2fcf4d7e77b2cbe19edb4d2ab8
Evgeny Varnakov <e@varnakov.ru>
2016-09-17 14:06:07 +0300

    Move subentities inclusion criteria to API comtrollers

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::QuestionsController < Api::V1::ApiController
   def index
     @questions = Question.all
-    respond_with @questions
+    respond_with @questions, include: nil
   end
 
   def show
--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,13 +1,9 @@
 class QuestionSerializer < ActiveModel::Serializer
   attributes :id, :topic, :short_title, :body, :created_at, :updated_at
 
-  has_many :answers, if: :include_accociations?
-  has_many :attachments, if: :include_accociations?
-  has_many :comments, if: :include_accociations?
+  has_many :answers
+  has_many :attachments
+  has_many :comments
 
   def short_title; object.topic.truncate(10); end
-
-  def include_accociations?
-    instance_options[:template] == 'show'
-  end
 end
