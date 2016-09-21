3be826ffdaacff91066e8ca4c43c1139e85217df
Evgeny Varnakov <e@varnakov.ru>
2016-09-22 00:33:24 +0300

    Fix Answer mailer and its spec

--- spec/jobs/mail_answer_job_spec.rb
+++ spec/jobs/mail_answer_job_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 describe MailAnswerJob do
-  DatabaseCleaner.clean!
   let(:user) { create(:user) }
-  let(:question) { create(:question) }
+  let(:alt_user) { create(:user) }
+  let(:question) { create(:question, user: alt_user) }
   let!(:subscription) { create(:subscription, user: user, question: question) }
-  let(:answer) { create(:answer, question:question) }
+  let(:answer) { create(:answer, question: question) }
   let(:message) { double(CustomMailer.delay) }
 
-  it 'should send new Answer notifications' do
-    expect(CustomMailer).to receive(:answer).with(user, answer).and_return(message)
-    expect(message).to receive(:deliver_later)
+  it 'should send new Answer notifications for both User and question author' do
+    [user, alt_user].each do |u|
+      expect(CustomMailer).to receive(:answer).with(u, answer).and_return(message)
+      expect(message).to receive(:deliver_later)
+    end
     MailAnswerJob.perform_now(answer)
   end
 
