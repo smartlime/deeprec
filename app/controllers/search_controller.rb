9de295f534daa0c1133abab15a994b0e13c9698f
Evgeny Varnakov <e@varnakov.ru>
2016-09-23 02:39:38 +0300

    Search implementation w/o specs and indices rebuild hooks

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,22 @@ class SearchController < ApplicationController
   before_filter :get_search_params
 
   def search
+    @found = search_class.search(@search_query)
+    render :no_results if @found.empty?
   end
 
   private
+
+  def search_class
+    puts "ST: #{@search_type}"
+    case @search_type
+      when '' then ThinkingSphinx
+      when 'q' then Question
+      when 'a' then Answer
+      when 'c' then Commient
+      when 'u' then User
+    end
+  end
 
   def get_search_params
     @search_query = params[:q]
