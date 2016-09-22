7d552bb1878a582751a33542d50b2e271034355f
Evgeny Varnakov <e@varnakov.ru>
2016-09-22 17:41:43 +0300

    Forgotten mailer preview

--- spec/mailers/previews/custom_mailer_preview.rb
+++ spec/mailers/previews/custom_mailer_preview.rb
@@ -3,12 +3,15 @@ class CustomMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/custom_mailer/digest
   def digest
-    CustomMailer.digest
+    raise "No users, cannot create sample" if User.count == 0
+    raise "No questions, cannot create sample" if Question.count == 0
+    CustomMailer.digest(User.take, Question.limit(5).map(&:id))
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/custom_mailer/answer
   def answer
-    CustomMailer.answer
+    question = Question.joins(:subscriptions, :answers).first
+    raise "No subsriptions, cannot create sample" if question.nil?
+    CustomMailer.answer(question.user, question.answers.first)
   end
-
 end
