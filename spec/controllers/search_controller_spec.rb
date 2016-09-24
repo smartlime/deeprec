98e8e0badf677786524b8d6d0a4b39832e2cf540
Evgeny Varnakov <e@varnakov.ru>
2016-09-24 11:27:17 +0300

    SearchController with spec ans shared examples

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,65 +1,11 @@
 require 'rails_helper'
 
-shared_examples :search_some do |object|
-  # let!(:object) { create(:question) }
-
-  it 'should call SearchService with corrent params' do
-    expect(SearchService).to receive(:klass).with('q').and_return(Question)
-    expect(SearchService).to receive(:search).with(type: 'q', query: object.topic).and_call_original
-    get :search, q: object.topic, t: 'q'
-  end
-
-  context 'calling search on empty database' do
-    before { get :search, q: object.topic, t: 'q' }
-    subject { response }
-
-    it 'should set @found to empty Array' do
-      get :search, q: object.topic, t: 'q'
-      expect(assigns(:found)).to eq []
-    end
-
-    it { is_expected.to have_http_status :success }
-    it { is_expected.to render_template :no_results }
-  end
-end
-
 describe SearchController do
-   describe 'GET #search' do
-
-     include_examples :search_some, let!(:question) { create(:question) }
-
-    #   let!(:object) { create(:question) }
-    #
-    #   # context 'when query is invalid' do
-    #   #   subject { get :search, t: 'ZZZ' }
-    #   #
-    #   #   it 'shouldn\'t set @found'
-    #   #
-    #   #   it 'shouldn\'t performs search using Sphinx'
-    #   #
-    #   #   subject { response }
-    #   #   it { is_expected.to have_http_status :success }
-    #   #   it { is_expected.to render_template :no_results }
-    #   # end
-    #
-    #   it 'should call SearchService with corrent params' do
-    #     #question = double(:question, title: Faker::Lorem.sentence)
-    #     expect(SearchService).to receive(:klass).with('q').and_return(Question)
-    #     expect(SearchService).to receive(:search).with(type: 'q', query: object.topic).and_call_original
-    #     get :search, q: object.topic, t: 'q'
-    #   end
-    #
-    #   context 'calling search on empty database' do
-    #     before { get :search, q: object.topic, t: 'q' }
-    #     subject { response }
-    #
-    #     it 'should set @found to empty Array' do
-    #       get :search, q: object.topic, t: 'q'
-    #       expect(assigns(:found)).to eq []
-    #     end
-    #
-    #     it { is_expected.to have_http_status :success }
-    #     it { is_expected.to render_template :no_reqults }
-    #   end
+  describe 'GET #search' do
+    include_examples :search_with_sphinx, :question, :topic
+    include_examples :search_with_sphinx, :question
+    include_examples :search_with_sphinx, :answer
+    include_examples :search_with_sphinx, :comment
+    include_examples :search_with_sphinx, :user, :email
   end
 end
