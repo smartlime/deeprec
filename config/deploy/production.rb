9c2a7f9044337712d9119e345698e111ccc7026b
Evgeny Varnakov <e@varnakov.ru>
2016-09-27 19:35:55 +0300

    Minor config changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,7 @@ server 'deeprec.davelabs.ru', port: 4242, user: 'deploy', roles: %w{app db web},
 
 set :rails_env, :production
 set :enable_ssl, false
+set :rvm_ruby_version, '2.3.1@deeprec'
 
 set ssh_options: {
     keys: %w(~/.ssh/github_rsa),
