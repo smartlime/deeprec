8c281c1caabef1a66604124cfb0682a1cdd4e4bf
Evgeny Varnakov <e@varnakov.ru>
2016-09-27 21:17:25 +0300

    Add public/uploads to shared files

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :bundle_flags, '--deployment'
 set :format, :airbrussh
 
 append :linked_files, 'config/database.yml', 'config/private_pub.yml', 'config/secrets.yml', '.env', '.ruby-gemset'
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads'
 
 namespace :deploy do
   after :finishing, 'deploy:cleanup'
