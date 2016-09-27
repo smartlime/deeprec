57d89f427fdad40d2d4c94092f9068e3222348ae
Evgeny Varnakov <e@varnakov.ru>
2016-09-27 19:47:29 +0300

    Forgot to remove '--quiet' from remote bunler execution

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, 'git@github.com:geneux/deep_recursion.git'
 set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 set :deploy_to, '/www/deeprec'
+set :bundle_flags, '--deployment'
 set :format, :airbrussh
 
 append :linked_files, 'config/database.yml', 'config/private_pub.yml', 'config/secrets.yml', '.ruby-gemset'
