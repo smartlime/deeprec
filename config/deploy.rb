00e986369dc4cb19431eb7c59c441e0647def166
Evgeny Varnakov <e@varnakov.ru>
2016-09-27 21:42:31 +0300

    Add Private Pub to prod

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,8 @@ set :deploy_to, '/www/deeprec'
 set :bundle_flags, '--deployment'
 set :format, :airbrussh
 
-append :linked_files, 'config/database.yml', 'config/private_pub.yml', 'config/secrets.yml', '.env', '.ruby-gemset'
+append :linked_files, 'config/database.yml', 'config/private_pub.yml', 'config/private_pub.prod.yml',
+       'config/secrets.yml', '.env', '.ruby-gemset', 'config/thinking_sphinx.yml'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads'
 
 namespace :deploy do
